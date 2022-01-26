ad357e22e8667f9cb5fe6e2e380d36b2e556f26c
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-01-26 18:06:13 -0300

    Requisito 1 pronto

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -41,11 +41,28 @@ export default function Table() {
           <th>Edited</th>
           <th>URL</th>
         </tr>
-        <tr>
+        {/* <tr>
           {
             data.map((planet) => <td key={ planet.name }>{planet.name}</td>)
           }
-        </tr>
+        </tr> */}
+        {data.map((planet, key) => (
+          <tr key={ key }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
       </table>
       {console.log(data)}
     </div>);
